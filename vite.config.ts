0bef105fb73506159873189cf11b0be014b7637a
BrunoFeitosa5 <bruno.cultura2015@outlook.com>
2025-09-05 11:14:24 -0300

    fix: base path for netlify and github pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ mode }) => ({
+export default defineConfig(() => ({
   plugins: [react()],
-  base: mode === 'production'
-    ? '/Filadelfia-celula/' // GitHub Pages
-    : '/',                  // Netlify e dev
+  base: process.env.NETLIFY === 'true'
+    ? '/'                  // Netlify (produção e dev)
+    : '/Filadelfia-celula/', // GitHub Pages
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
